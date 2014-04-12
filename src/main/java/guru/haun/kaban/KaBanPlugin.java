833488da8202ce690ee7905486145edda9df13b8
Aaron Haun <kj4ips@antichthon.net>
2014-04-12 16:23:59 -0400

    Redined POM some more, moved plugin.yml where it needs to go

--- src/main/java/guru/haun/kaban/KaBanPlugin.java
+++ src/main/java/guru/haun/kaban/KaBanPlugin.java
@@ -1,7 +1,16 @@
 package guru.haun.kaban;
 
+import java.util.logging.Level;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KaBanPlugin extends JavaPlugin {
-
+	@Override
+	public void onEnable(){
+		this.getLogger().log(Level.WARNING, "This is a development version of KaBan, USE AT YOUR OWN RISK!");
+	}
+	
+	public void onDisable(){
+		
+	}
 }
